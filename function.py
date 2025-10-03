9c21ec2ce24fc4f11b0d6873c2ece3880d7b7f3e
maksim <maksim.filimonov@voco.ee>
2025-10-03 11:50:20 +0300

    zadanie na funktsiju Mihh

---
+++ function.py
@@ -0,0 +1,26 @@
+"""Function examples."""
+
+
+def func():
+    """Print I´m inside the function."""
+    print('I´m inside the function')
+
+
+def my_name_is(name):
+    """Print name."""
+    print(f'My name is {name}')
+
+
+def sum_six(num):
+    """Return num + 6."""
+    return 6 + num
+
+
+def sum_numbers(a, b):
+    """Return sum."""
+    return a + b
+
+
+def usd_to_eur(d):
+    """Return euro."""
+    return d * 0.8
